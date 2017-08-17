c3b723c33276dbfb96aa421fc858b611488ee1a7
Mohit Singh <mohitsinghs.dev@gmail.com>
2017-08-17 21:43:32 +0530

    Use yaml data directory

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ const del = require('del')
 const bs = require('browser-sync')
 const gulpLoadPlugins = require('gulp-load-plugins')
 const critical = require('critical')
+const dataDir = require('data-dir')
 const $ = gulpLoadPlugins()
 const reload = bs.reload
 const cs = critical.stream
 
-module.exports = function (_gulp) {
+module.exports = _gulp => {
   const defaults = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'njk_defaults.yml'), 'utf8'))
   const options = _.defaultsDeep(yaml.safeLoad(fs.readFileSync('njk.yml', 'utf8')) || {}, defaults)
   const runSequence = require('run-sequence').use(_gulp)
 
   // Optimize Images
-  _gulp.task('images', function () {
+  _gulp.task('images', () => {
     const plugins = Object.keys(options.images.imagemin)
     const defaultPlugins = ['pngquant', 'jpegtran', 'gifsicle', 'svgo']
     let imageminOptions = []
-    plugins.forEach(function (plugin) {
+    plugins.forEach(plugin => {
       if (_.has(defaultPlugins, plugin)) imageminOptions.push($.imagemin[plugin](options.images.imagemin[plugin]))
       else imageminOptions.push(require(`imagemin-${plugin}`)(options.images.imagemin[plugin]))
     }, this)
@@ -33,11 +34,11 @@ module.exports = function (_gulp) {
   })
 
   // Store SVG Images
-  _gulp.task('svg', function () {
+  _gulp.task('svg', () => {
     return _gulp.src(options.svg.src)
       .pipe($.svgstore({inlineSvg: true}))
       .pipe($.cheerio({
-        run: function ($) {
+        run: $ => {
           $('svg').attr('style', 'display:none')
         },
         parserOptions: {xmlMode: true}
@@ -46,7 +47,7 @@ module.exports = function (_gulp) {
   })
 
   // Generate Styles
-  _gulp.task('styles', function () {
+  _gulp.task('styles', () => {
     return _gulp.src(options.styles.src)
       .pipe($.plumber())
       .pipe($.sass(options.styles.sass).on('error', $.sass.logError))
@@ -58,7 +59,7 @@ module.exports = function (_gulp) {
   })
 
   // Generate Scripts
-  _gulp.task('scripts', function () {
+  _gulp.task('scripts', () => {
     return _gulp.src(options.scripts.src)
       .pipe($.plumber())
       .pipe($.concat(options.scripts.outfile))
@@ -68,7 +69,10 @@ module.exports = function (_gulp) {
   })
 
   // Render Nunjuks to HTML
-  _gulp.task('html', ['styles', 'scripts', 'svg'], function () {
+  _gulp.task('html', ['styles', 'scripts', 'svg'], () => {
+    if (options.pages.dataDir) {
+      options.pages.nunjucks.data = dataDir(options.pages.dataDir).next().value
+    }
     return _gulp.src(options.pages.src)
       .pipe($.plumber())
       .pipe($.nunjucksMd(options.pages.nunjucks))
@@ -83,7 +87,7 @@ module.exports = function (_gulp) {
   })
 
   // Search Engine Optimization
-  _gulp.task('seo', ['html'], function () {
+  _gulp.task('seo', ['html'], () => {
     return _gulp.src(options.seo.src)
       .pipe($.plumber())
       .pipe($.save('sitemap-save'))
@@ -102,8 +106,8 @@ module.exports = function (_gulp) {
   })
 
   // Watch file changes
-  _gulp.task('serve', function () {
-    return runSequence('clean', ['html', 'images'], function () {
+  _gulp.task('serve', () => {
+    return runSequence('clean', ['html', 'images'], () => {
       bs(options.watch.browserSync)
       _gulp.watch(options.watch.scripts, ['scripts'])
       _gulp.watch(options.watch.styles, ['styles'])
@@ -122,7 +126,7 @@ module.exports = function (_gulp) {
   })
 
   // Clean bulid directory
-  _gulp.task('clean', function () {
+  _gulp.task('clean', () => {
     return del([options.build.dir])
   })
 
@@ -133,7 +137,7 @@ module.exports = function (_gulp) {
   })
 
   // Clean and build
-  _gulp.task('default', function () {
+  _gulp.task('default', () => {
     return runSequence('clean', 'build')
   })
 }
