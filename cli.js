c73cb7f1b94a03bfc30dfb51d68040f334b4a7c5
Mohit Singh <mohitsinghs.dev@gmail.com>
2017-12-14 21:02:20 +0530

    Minor tweaks and fixes

--- cli.js
+++ cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 'use strict'
+const fs = require('fs')
+const path = require('path')
 const meow = require('meow')
-const readFiles = require('./lib/read-files')
-const readData = require('./lib/read-data')
+const ora = require('ora')
+const deepFiles = require('deep-files')
+const dataDir = require('data-dir')
 const njk = require('./')
 
 const cli = meow(`
@@ -12,10 +15,10 @@ const cli = meow(`
 
   Options
     --data, -d                JSON data
-    --template, -t            Template folder
+    --template, -t            Template directory
     --use-block, -b           Content block in files
     --escape-markdown, -e     Escape markdown
-    --out, -o                 Output folder
+    --out, -o                 Output directoty
 
     If no option is passed, current directory is used
 
@@ -49,4 +52,33 @@ const cli = meow(`
   }
 })
 
-njk(readFiles(cli.input), readData(cli.flags.d), cli.flags)
+const PATTERN = /\.njk|\.html|\.md|\.mdown|\.markdown/
+let current = cli.input.length ? cli.input[0] : process.cwd()
+const spinner = ora('Processing').start()
+
+function readData () {
+  if (cli.flags.d) {
+    try {
+      if (fs.lstatSync(cli.flags.d).isDirectory()) {
+        return dataDir(path.resolve(cli.flags.d))
+      } else {
+        return JSON.parse(fs.readFileSync(cli.flags.d))
+      }
+    } catch (err) {
+      spinner.fail(`Error reading data from ${cli.flags.d}`)
+      process.exit(1)
+    }
+  } else {
+    spinner.warn('Using empty global data')
+    return {}
+  }
+}
+
+if (cli.input.length > 1) {
+  cli.input.forEach(file => {
+    njk(deepFiles(file, PATTERN), readData(), cli.flags)
+  })
+} else {
+  njk(deepFiles(current, PATTERN), readData(), cli.flags)
+}
+spinner.stop()
