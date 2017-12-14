e8349d8f0040d6fd4951a976bec40473842324ff
Mohit Singh <mohitsinghs.dev@gmail.com>
2017-12-14 21:02:20 +0530

    Minor tweaks and fixes

--- index.js
+++ index.js
@@ -1,23 +1,85 @@
+const fs = require('fs')
 const path = require('path')
+const _ = require('lodash')
 const ora = require('ora')
-const resolve = require('./lib/resolve-file')
-const wrap = require('./lib/wrap-layout')
-const render = require('./lib/render-template')
-const md = require('./lib/process-markdown')
+const frontMatter = require('front-matter')
+const marked = require('marked')
+const nunjucks = require('nunjucks')
 
 module.exports = (files, data, flags) => {
-  files.forEach(file => {
-    let spinner = ora(`Processing ${file}`).start()
-    Promise
-    .resolve(file)
-    .then(file => resolve(file, data))
-    .then(file => md(file, flags.escapeMarkdown))
-    .then(file => wrap(file, flags.useBlock))
-    .then(file => render(file, flags.out, flags.template))
-    .then(name => spinner.succeed(`Done ${name}`))
-    .catch(err => {
-      spinner.fail(`Error processing ${path.parse(file).base}\n${err}`)
-      process.exit(1)
+  if (Array.isArray(files)) {
+    files.forEach(file => {
+      _processFile(file)
     })
-  })
+  } else {
+    _processFile(files)
+  }
+
+  function _processFile (file) {
+    let spinner = ora(`Processing ${path.parse(file).base}`).start()
+    return _resolveFile(file)
+      .then(file => _processMarkdown(file))
+      .then(file => _wrapLayout(file))
+      .then(file => _renderTemplate(file))
+      .then(result => spinner.succeed(`Written ${result}`))
+      .catch(err => {
+        spinner.fail(err)
+        process.exit(1)
+      })
+  }
+
+  function _resolveFile (file) {
+    return new Promise((resolve, reject) => {
+      try {
+        const fm = frontMatter(fs.readFileSync(path.resolve(file)).toString())
+        resolve({
+          data: _.merge({}, data, {page: fm.attributes}),
+          content: fm.body,
+          info: path.parse(file),
+          haveAttributes: !_.isEmpty(fm.attributes)
+        })
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+  function _processMarkdown (file) {
+    if (/\.md|\.mdown|\.markdown/.test(file.info.ext)) {
+      const render = new marked.Renderer()
+      render.text = text => unescape(text)
+      file.content = flags.escapeMarkdown ? marked(file.content) : marked(file.content, {renderer: render})
+      return file
+    }
+    return file
+  }
+
+  function _wrapLayout (file, useBlock) {
+    if (_.has(file.data, 'page.layout')) {
+      const canUseBlock = _.has(file.data, 'page.useBlock') ? file.data.page.useBlock : flags.useBlock
+      const extendLayout = `{% extends "${file.data.page.layout}.njk" %}`
+      const extendBlock = `{% block content %}${file.content}{% endblock %}`
+      file.content = `${extendLayout} ${canUseBlock ? extendBlock : file.content}`
+      return file
+    } else if (file.haveAttributes) {
+      throw Error(`No layout declared in front-matter or data`)
+    }
+    return file
+  }
+
+  function _renderTemplate (file) {
+    const compile = new nunjucks.Environment(new nunjucks.FileSystemLoader(flags.t))
+    try {
+      fs.lstatSync(flags.o).isDirectory()
+    } catch (err) {
+      fs.mkdirSync(path.resolve(flags.o))
+    }
+    try {
+      const result = compile.renderString(file.content, file.data)
+      fs.writeFileSync(path.join(flags.o, `${file.info.name}.html`), result)
+    } catch (err) {
+      throw Error(`Error rendering ${file.info.base} \n ${err}`)
+    }
+    return file.info.base
+  }
 }
